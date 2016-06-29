4e1b55dfeeaee9c23ce43d137e866e02322d42e8
zhangxi <zhangxi_1989@sina.com>
2016-06-29 23:08:32 +0800

    init

---
+++ SVG.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*
+ 
+ http://www.w3school.com.cn/svg/svg_circle.asp
+ 
+ 
+<rect width="30" height="100" style="fill:rgb(255,255,55);stroke-width:1;stroke:rgb(200,50,100)" />
+ 
+ 矩形 <rect>
+ 圆形 <circle>
+ 椭圆 <ellipse>
+ 线 <line>
+ 折线 <polyline>
+ 多边形 <polygon>
+ 路径 <path>
+ */
+
+
+class SVGRect
+{
+    /*
+     width 和 height
+     CSS 的 fill 属性定义矩形的填充颜色（rgb 值、颜色名或者十六进制值）
+     CSS 的 stroke-width 属性定义矩形边框的宽度
+     CSS 的 stroke 属性定义矩形边框的颜色
+     x 属性定义矩形的左侧位置（例如，x="0" 定义矩形到浏览器窗口左侧的距离是 0px）
+     y 属性定义矩形的顶端位置（例如，y="0" 定义矩形到浏览器窗口顶端的距离是 0px）
+     CSS 的 fill-opacity 属性定义填充颜色透明度（合法的范围是：0 - 1）
+     CSS 的 stroke-opacity 属性定义笔触颜色的透明度（合法的范围是：0 - 1）
+     rx 和 ry 属性可使矩形产生圆角。
+     
+     
+     circle
+     cx 和 cy 属性定义圆点的 x 和 y 坐标。如果省略 cx 和 cy，圆的中心会被设置为 (0, 0)
+     */
+}
